3ec2c82a58be9faf0510a7ee2b1eaf05fccc05f0
Ch33rsL0ve <ch33rsl0ve4l0ve@gmail.com>
2022-07-04 12:30:30 +0300

    Movement+Jump+Dash

--- Assets/Scripts/Environment/Ground.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Ground : MonoBehaviour
-{
-   [SerializeField] private bool _isJumpable = true;
-   public bool debug = true;
-   public bool IsJumpable => _isJumpable;
-}
